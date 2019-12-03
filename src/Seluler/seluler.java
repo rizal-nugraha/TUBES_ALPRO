cad74bd96a105b4b75ef53dd05c54c98ecd4d38c
rizal-nugraha <rizal.nugrah@gmail.com>
2019-12-03 19:57:47 +0700

    tambah data simcard

--- src/Seluler/seluler.java
+++ src/Seluler/seluler.java
@@ -14,6 +14,7 @@ public class seluler {
 static Scanner sc = new Scanner(System.in);
 static StringBuffer stringBufferOfdata = new StringBuffer();
 
+<<<<<<< HEAD
 public static void tambahSimcard() {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	BufferedWriter outFile;
@@ -32,6 +33,8 @@ public static void tambahSimcard() {
 	}
 	
 }
+=======
+>>>>>>> tambah data simcard
 
 public static void tampilData() {
 	
